706f47bf54571ac85fde0ca1ebabc3c4678f86a8
Renato Filho <renatosousafilho@gmail.com>
2024-06-26 11:30:11 -0300

    refactor: muda nome da env var

--- src/app/service/api.ts
+++ src/app/service/api.ts
@@ -1,6 +1,7 @@
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const getAllPassengers = async () => {
+  console.log(BASE_URL);
   const response = await fetch(`${BASE_URL}/passengers`);
   const data = await response.json();
   return data;
